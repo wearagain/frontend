78ecab9273ffff89c72d59913d943810049fa62f
Whatdoyumin <lym4963@naver.com>
2025-10-14 14:38:53 +0900

    chore: 프로젝트 환경 설정 및 라이브러리 추가

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>다시입다 연구소</div>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "@/lib/queryClient";
+import { router } from "@/routes";
+import "@/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>,
 );

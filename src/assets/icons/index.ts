642aa37880f4e8c03f4ac119964ce93cdc438ad4
Whatdoyumin <lym4963@naver.com>
2025-10-20 00:45:09 +0900

    fix: 아이콘 오류 해결

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,1 +1,4 @@
+export { default as GIcon } from "./GIcon";
+export { default as KIcon } from "./KIcon";
+export { default as NIcon } from "./NIcon";
 export { default as ProfileIcon } from "./ProfileIcon";
